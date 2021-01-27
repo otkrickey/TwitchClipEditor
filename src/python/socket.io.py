c45e7fd7aa01f13362db756476dd8f30c8788392
otkrickey <rtk.otk@gmail.com>
2021-01-27 23:11:12 +0900

    updated logger

--- src/python/socket.io.py
+++ src/python/socket.io.py
@@ -7,45 +7,40 @@ app = socketio.WSGIApp(sio, static_files={
     '/socket.io.js': 'C:/Users/rtkfi/OneDrive/デスクトップ/TwitchClipEditor/src/javascript/socket.io.js',
 })
 
-client_sid: dict = {}
+client_sid: dict[str] = {'node': None, 'web': None, }
 
 
-def log(value: str, *args: str):
-    _ = ''
-    for s in args: _ += '[%s]' % s
-    _ += ' %s' % value
+def log(sid: str, value: str, *args: str, ):
+    _ = ''.join(['[%s]' % s for s in args])
+    sio.emit('python_logger', value, room=sid)
     print(_)
     return _
 
 
-def exe_edit(sid, value):
-    log('Server Started', 'python', 'ctrl')
+def exe_edit(value: int) -> None:
+    log(client_sid['node'], 'Server Opened', 'python', 'logger')
     for i in range(value):
-        sio.emit('python_executing', i, room=client_sid['web_client'])
-    log('Server Finished', 'python', 'ctrl')
+        sio.emit('python_executing', i, room=client_sid['web'])
+    log(client_sid['node'], 'Server Opened', 'python', 'logger')
 
 
 @sio.event
-def connect(sid, environ):
+def connect(sid: str, environ):
+    sio.emit('python_logger', 'connected to %s' % sid, room=sid)
     print('connect ', sid)
 
 
 @sio.event
-def define_client(sid, value):
+def define_client(sid: str, value):
     client_sid[value] = sid
     print('[python][sid] => ' + json.dumps(client_sid))
 
 
 @sio.event
-def python_start_request(sid, value):
+def python_start_request(sid: str, value):
     print('[python][ctrl] => `Request accepted.`')
-    sio.emit('python_start_request', '[python][ctrl] => `Request accepted.`', room=client_sid['nodejs_client'])
-    exe_edit(sid, value)
-
-
-@sio.event
-def msg(sid, data):
-    print('[python][ctrl] => %s' % data)
+    sio.emit('python_start_request', '[python][ctrl] => `Request accepted.`', room=client_sid['nodejs'])
+    exe_edit(value)
 
 
 @sio.event
