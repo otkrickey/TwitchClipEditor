121cad5dcd440006fbe3cb4b9dca6fb4210c1701
otkrickey <rtk.otk@gmail.com>
2021-01-28 07:55:45 +0900

    Improved readability

--- src/python/createImage.py
+++ src/python/createImage.py
@@ -56,15 +56,28 @@ class Rect:
 
 
 class Text:
-    def __init__(self, content, fontsize, color):
-        PIL_Image = Image.fromarray(self.g[..., :3])
+    def __init__(self, size: Tuple[int, int], color: Tuple[int, int, int], content: str, fontsize: Union[int, float]) -> None:
+        self.canvas = np.full((size[1], size[0], 4), 0, dtype=np.uint8)
+        PIL_Image = Image.fromarray(self.canvas[..., :3])
         draw = ImageDraw.Draw(PIL_Image)
         font = ImageFont.truetype(".fonts/MPLUSRounded1c-Medium.ttf", fontsize)
-        t_s = draw.textsize(content, font=font)
-        t_o = font.getoffset(content)
-        px = (self.s[0] - t_s[0] - t_o[0]) / 2
-        py = (self.s[1] - t_s[1] - t_o[1]) / 2
+        textsize = draw.textsize(content, font=font)
+        font_offset = font.getoffset(content)
+        px = (self.s[0] - textsize[0] - font_offset[0]) / 2
+        py = (self.s[1] - textsize[1] - font_offset[1]) / 2
         draw.text((px, py), content, color, font)
-        self.g[..., :3] = np.array(PIL_Image, dtype=np.uint8)
-        self.g[..., 3] = self.g[..., :3].sum(axis=2) / sum(color) * 255
+        self.canvas[..., :3] = np.array(PIL_Image, dtype=np.uint8)
+        self.canvas[..., 3] = self.canvas[..., :3].sum(axis=2) / sum(color) * 255
         return self
+
+    @property
+    def image(self) -> np.ndarray:
+        """
+        文字の画像の出力
+
+        Returns
+        -------
+        canvas : np.ndarray
+            作成された画像のコピーデータを出力。
+        """
+        return self.canvas.copy()
