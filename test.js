1d04564eb3025dc594c160446d22daf5075dc1ac
otkrickey <rtk.otk@gmail.com>
2021-01-28 02:58:36 +0900

    deleted test file

--- test.js
+++
@@ -1,6 +0,0 @@
-
-function log(value, ...args) {
-    console.log((function (args) { let str = ''; args.forEach(function (e) { str += `[${e}]` }); return str })(args) + ' ' + String(value));
-}
-
-log('started', 'python', 'console');
