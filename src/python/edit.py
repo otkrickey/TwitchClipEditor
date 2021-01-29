6407c7e7fb50cdd652cf52c5ae88b98ee718d858
otkrickey <rtk.otk@gmail.com>
2021-01-29 18:57:27 +0900

    Add 'Edit' class

--- src/python/edit.py
+++ src/python/edit.py
@@ -1,3 +1,4 @@
+import subprocess
 from typing import Tuple, Union
 
 import cv2
@@ -8,7 +9,7 @@ from src.python.easing import Easing
 
 
 class Image:
-    def __init__(self, image: np.ndarray, timing: Tuple[int, int], start_position: Tuple(int, int), end_position: Tuple(int, int)) -> None:
+    def __init__(self, image: np.ndarray, timing: tuple[int, int], start_position: tuple, end_position: tuple) -> None:
         self.image = image
         self.st = timing[0]
         self.et = timing[1]
@@ -17,7 +18,7 @@ class Image:
         self.epx = end_position[0]
         self.epy = end_position[1]
 
-    def position(self, t) -> Tuple[np.ndarray, Tuple[int, int]]:
+    def position(self, t) -> tuple[np.ndarray, tuple]:
         if t < self.st:
             return (self.image, (self.spx, self.spy))
         elif self.st <= t < self.et:
@@ -27,8 +28,8 @@ class Image:
             return (self.image, (self.epx, self.epy))
 
 
-def mask(back: np.ndarray, front: np.ndarray, position: Tuple[int, int]) -> np.ndarray:
-    """
+def mask(back: np.ndarray, front: np.ndarray, position: tuple) -> np.ndarray:
+    '''
     `back`に指定された画像に`front`に指定した画像を`position`で指定した位置に合成する。
 
     Parameters
@@ -37,7 +38,7 @@ def mask(back: np.ndarray, front: np.ndarray, position: Tuple[int, int]) -> np.n
         背景画像。後ろ側の画像。
     front : np.ndarray
         合成画像。手間側の画像。
-    position : Tuple[int, int]
+    position : tuple
         合成する位置。`back`に指定した画像の左上を基準に横軸x、縦軸yで指定する。
         `front`に指定した画像の左上の場所を指す。
 
@@ -45,7 +46,7 @@ def mask(back: np.ndarray, front: np.ndarray, position: Tuple[int, int]) -> np.n
     -------
     back : np.ndarray
         合成後の背景画像。
-    """
+    '''
     px, py = position
     fh: int = front.shape[0]
     fw: int = front.shape[1]
@@ -60,7 +61,7 @@ def mask(back: np.ndarray, front: np.ndarray, position: Tuple[int, int]) -> np.n
         return back
     front3: np.ndarray = front[..., : 3]
     mask1: np.ndarray = front[..., 3]
-    mask3: np.ndarray = cv2.merge(mask1, mask1, mask1)
+    mask3: np.ndarray = cv2.merge((mask1, mask1, mask1))
     mask_roi: np.ndarray = mask3[py1 - py: py2 - py, px1 - px: px2 - px]
     front_roi: np.ndarray = front3[py1 - py: py2 - py, px1 - px: px2 - px]
     roi: np.ndarray = back[py1:py2, px1:px2]
@@ -68,3 +69,69 @@ def mask(back: np.ndarray, front: np.ndarray, position: Tuple[int, int]) -> np.n
     tmp2: np.ndarray = cv2.bitwise_or(tmp1, front_roi)
     back[py1:py2, px1:px2] = tmp2
     return back
+
+
+class Edit:
+    def __init__(self, clip_id: int, textContent: str, FILE_PATH: str) -> None:
+        filename = f'clip-{clip_id}.mp4'
+        self.filename = filename
+        self.FILE_PATH = FILE_PATH
+        self.InputVideo = cv2.VideoCapture(f'{FILE_PATH}download/{self.filename}')
+        self.OutPutVIdeo = cv2.VideoWriter(f'{FILE_PATH}export/{self.filename}', cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), 60, (1920, 1080))
+        self.FrameCount = int(self.InputVideo.get(cv2.CAP_PROP_FRAME_COUNT))
+        # call 3 images here
+        self.Rect1 = Rect((360, 120), (197, 22, 92), radius=8).image
+        self.Rect2 = Rect((360, 10), (255, 255, 255), radius=5).image
+        self.Text3 = Text((360, 120), (255, 255, 255), textContent).image
+        self.FrontImage1 = Image(self.Rect1, (0, 60), (-340, 60), (20, 60))
+        self.FrontImage2 = Image(self.Rect2, (15, 75), (-340, 170), (20, 170))
+        self.FrontImage3 = Image(self.Text3, (30, 90), (-340, 60), (20, 60))
+
+    def edit(self):
+        for i in range(self.FrameCount):
+            flag, frame = self.InputVideo.read()
+            if not flag: break
+            frame = mask(frame, *self.FrontImage1.position(i))
+            frame = mask(frame, *self.FrontImage2.position(i))
+            frame = mask(frame, *self.FrontImage3.position(i))
+            self.OutPutVIdeo.write(frame)
+            if i % 100 == 0: print(i)
+        self.InputVideo.release()
+        self.OutPutVIdeo.release()
+        subprocess.call(
+            f'ffmpeg -i {self.FILE_PATH}export/{self.filename} -i {self.FILE_PATH}download/{self.filename} -c:v copy {self.FILE_PATH}{self.filename} -loglevel quiet -y'
+        )
+
+
+FILE_PATH = 'src/video/'
+
+
+def edit(self, clip_id: int, textContent: str) -> None:
+    filename = f'clip-{clip_id}.mp4'
+    INPUT_VIDEO = cv2.VideoCapture(f'{FILE_PATH}download/{filename}')
+    OUTPUT_VIDEO = cv2.VideoCapture(f'{FILE_PATH}export/{filename}', cv2.VideoWriter_fourcc('m', 'p', '4', 'v'), 60, (1920, 1080))
+    FrameCount = int(INPUT_VIDEO.get(cv2.CAP_PROP_FRAME_COUNT))
+    RectImage1 = Rect((360, 120), (197, 22, 92), radius=8).image
+    RectImage2 = Rect((360, 10), (255, 255, 255), radius=5).image
+    TextImage3 = Text((360, 120), (255, 255, 255), textContent).image
+    FrontImage1 = Image(self.Rect1, (0, 60), (-340, 60), (20, 60))
+    FrontImage2 = Image(self.Rect2, (15, 75), (-340, 170), (20, 170))
+    FrontImage3 = Image(self.Text3, (30, 90), (-340, 60), (20, 60))
+    for i in range(FrameCount):
+        flag, frame = INPUT_VIDEO.read()
+        if not flag: break
+        frame = mask(frame, *FrontImage1.position(i))
+        frame = mask(frame, *FrontImage2.position(i))
+        frame = mask(frame, *FrontImage3.position(i))
+        OUTPUT_VIDEO.write(frame)
+    INPUT_VIDEO.release()
+    OUTPUT_VIDEO.release()
+    subprocess.call(f'ffmpeg -i {FILE_PATH}export/{filename} -i {FILE_PATH}download/{filename} -c:v copy {FILE_PATH}{filename} -loglevel quiet -y')
+
+
+def main():
+    Edit(991466429, 'Streamer Name', 'src/video/').edit()
+    pass
+
+
+if __name__ == '__main__': main()
