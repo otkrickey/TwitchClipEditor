04fd49b28361d17b4aa1d2c1817c793c10e860aa
otkrickey <rtk.otk@gmail.com>
2021-01-30 23:27:23 +0900

    Moved the Easing function

--- src/python/edit.py
+++ src/python/edit.py
@@ -6,7 +6,97 @@ import numpy as np
 from matplotlib import pyplot as plt
 from PIL import Image, ImageDraw, ImageFont
 
-from easing import Easing
+import math
+
+
+class Easing:
+    """
+    Easing Functions
+    ------
+
+    Methods:
+
+                      In: float  -     In ShortCut Function
+                     Out: float  -    Out ShortCut Function
+                   InOut: float  -  InOut ShortCut Function
+                  Bounce: float  - Bounce ShortCut Function
+
+              easeInSine: float  - easeInSine
+             easeOutSine: float  - easeOutSine
+           easeInOutSine: float  - easeInOutSine
+
+              easeInQuad: float  - easeInQuad
+             easeOutQuad: float  - easeOutQuad
+           easeInOutQuad: float  - easeInOutQuad
+
+             easeInCubic: float  - easeInCubic
+            easeOutCubic: float  - easeOutCubic
+          easeInOutCubic: float  - easeInOutCubic
+
+             easeInQuart: float  - easeInQuart
+            easeOutQuart: float  - easeOutQuart
+          easeInOutQuart: float  - easeInOutQuart
+
+             easeInQuint: float  - easeInQuint
+            easeOutQuint: float  - easeOutQuint
+          easeInOutQuint: float  - easeInOutQuint
+
+              easeInExpo: float  - easeInExpo
+             easeOutExpo: float  - easeOutExpo
+           easeInOutExpo: float  - easeInOutExpo
+
+              easeInCirc: float  - easeInCirc
+             easeOutCirc: float  - easeOutCirc
+           easeInOutCirc: float  - easeInOutCirc
+
+              easeInBack: float  - easeInBack
+             easeOutBack: float  - easeOutBack
+           easeInOutBack: float  - easeInOutBack
+
+           easeInElastic: float  - easeInElastic
+          easeOutElastic: float  - easeOutElastic
+        easeInOutElastic: float  - easeInOutElastic
+
+            easeInBounce: float  - easeInBounce
+           easeOutBounce: float  - easeOutBounce
+         easeInOutBounce: float  - easeInOutBounce
+    """
+
+    def __init__(self, x: float) -> None: self.x = x
+    def In(self, i: int) -> float: return self.x ** i
+    def Out(self, i: int) -> float: return 1 - (1 - self.x) ** i
+    def InOut(self, i: int) -> float: return 2 ** (i - 1) * self.x ** i if self.x < 0.5 else 1 - 2 ** (i - 1) * (1 - self.x) ** i
+    def Bounce(self, t: int) -> float: return 7.5625 * t ** 2 if (t < 1 / 2.75) else 7.5625 * (t - 1.5 / 2.75) ** 2 + 0.75 if (t < 2 / 2.75) else 7.5625 * (t - 2.25 / 2.75) ** 2 + 0.9375 if (t < 2.5 / 2.75) else 7.5625 * (t - 2.625 / 2.75) ** 2 + 0.984375
+    def easeInSine(self) -> float: return 1 - math.cos((self.x * math.pi) / 2)
+    def easeOutSine(self) -> float: return math.sin((self.x * math.pi) / 2)
+    def easeInOutSine(self) -> float: return -(math.cos(self.x * math.pi) - 1) / 2
+    def easeInQuad(self) -> float: return self.In(2)
+    def easeOutQuad(self) -> float: return self.Out(2)
+    def easeInOutQuad(self) -> float: return self.InOut(2)
+    def easeInCubic(self) -> float: return self.In(3)
+    def easeOutCubic(self) -> float: return self.Out(3)
+    def easeInOutCubic(self) -> float: return self.InOut(3)
+    def easeInQuart(self) -> float: return self.In(4)
+    def easeOutQuart(self) -> float: return self.Out(4)
+    def easeInOutQuart(self) -> float: return self.InOut(4)
+    def easeInQuint(self) -> float: return self.In(5)
+    def easeOutQuint(self) -> float: return self.Out(5)
+    def easeInOutQuint(self) -> float: return self.InOut(5)
+    def easeInExpo(self) -> float: return 0 if self.x == 0 else 2 ** (10 * self.x - 10)
+    def easeOutExpo(self) -> float: return 1 if self.x == 1 else 1 - 2 ** (-10 * self.x)
+    def easeInOutExpo(self) -> float: return 0 if self.x == 0 else 1 if self.x == 1 else 2 ** (20 * self.x - 11) if self.x < 0.5 else 1 - 2 ** (-20 * self.x + 9)
+    def easeInCirc(self) -> float: return 1 - math.sqrt(1 - self.x ** 2)
+    def easeOutCirc(self) -> float: return 1 - math.sqrt(1 - (1 - self.x) ** 2)
+    def easeInOutCirc(self) -> float: return (1 + math.sqrt(1 - 4 * (self.x if self.x < 0.5 else (1 - self.x)) ** 2)) / 2
+    def easeInBack(self) -> float: return (2.70158 * self.x - 1.70158) * self.x ** 2
+    def easeOutBack(self) -> float: return 1 + (2.70158 * self.x - 1) * (1 - self.x) ** 2
+    def easeInOutBack(self) -> float: return 2 * self.x ** 2 * (3.59491 * 2 * self.x - 2.59491) if self.x < 0.5 else 2 * (1 - self.x) ** 2 * (7.18982 * self.x - 4.59491) + 1
+    def easeInElastic(self) -> float: return 0 if self.x == 0 else 1 if self.x == 1 else -2 ** (10 * self.x - 10) * math.sin((self.x * 10 - 10.75) * math.pi * 1.5)
+    def easeOutElastic(self) -> float: return 0 if self.x == 0 else 1 if self.x == 1 else 2 ** (-10 * self.x) * math.sin((self.x * 10 - 0.75) * math.pi * 1.5) + 1
+    def easeInOutElastic(self) -> float: return 0 if self.x == 0 else 1 if self.x == 1 else -2 ** (20 * self.x - 11) * math.sin((20 * self.x - 11.125) * math.pi * 2.25) if self.x < 0.5 else 2 ** (-20 * self.x + 9) * math.sin((20 * self.x - 11.125) * math.pi * 2.25) + 1
+    def easeInBounce(self) -> float: return 1 - self.Bounce(1 - self.x)
+    def easeOutBounce(self) -> float: return self.Bounce(self.x)
+    def easeInOutBounce(self) -> float: return (1 - self.Bounce(1 - 2 * self.x)) / 2 if self.x < 0.5 else (1 + self.Bounce(2 * self.x - 1)) / 2
 
 
 class Rect:
@@ -43,7 +133,6 @@ class Rect:
         cv2.rectangle(self.canvas, (radius, radius), (self.size[0] - radius, self.size[1] - radius), self.color, radius * 2, cv2.LINE_AA)
         self.canvas[..., 3] = self.canvas[..., :3].sum(axis=2) / sum(self.color) * 255
 
-    @property
     def image(self) -> np.ndarray:
         """
         Export Image
@@ -98,7 +187,6 @@ class Text:
         self.canvas[..., :3] = np.array(PIL_Image, dtype=np.uint8)
         self.canvas[..., 3] = self.canvas[..., :3].sum(axis=2) / sum(color) * 255
 
-    @property
     def image(self) -> np.ndarray:
         """
         Export Image
