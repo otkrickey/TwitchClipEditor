5a585c14df00b63593baaeb62c4e7d1456d178d7
otkrickey <rtk.otk@gmail.com>
2021-02-01 07:39:06 +0900

    Create test file (Changing Socket.io Host)

---
+++ src/python/io-client.py
@@ -0,0 +1,23 @@
+import socketio
+
+sio = socketio.Client()
+
+
+@sio.event
+def connect():
+    sio.emit('define_client', 'python')
+
+
+@sio.event
+def my_message(data):
+    print('message received with ', data)
+    sio.emit('my response', {'response': 'my response'})
+
+
+@sio.event
+def disconnect():
+    print('disconnected from server')
+
+
+sio.connect('ws://localhost:8080')
+sio.wait()
